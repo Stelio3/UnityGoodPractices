7feb45092d0539782a4c244abfcc365948db3c2f
Pablo H <pablo.hernandez@nextlimit.com>
2024-01-14 11:51:48 +0000

    LiskovSubstitution

---
+++ Assets/SOLID/LiskovSubstitution/RailVehicle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailVehicle : IMovable
+{
+    public float speed = 100;
+    public virtual void GoForward()
+    {
+
+    }
+    public virtual void Reverse()
+    {
+
+    }
+}
